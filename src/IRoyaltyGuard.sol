9d9504bdcd9f172b514497c3aa6d9330befe88f2
koloz <zach.kolodny@gmail.com>
2022-11-22 13:39:43 -0500

    finished source

--- src/IRoyaltyGuard.sol
+++ src/IRoyaltyGuard.sol
@@ -1,17 +1,96 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.15;
 
+/// @title IRoyaltyGuard
+/// @author highland, koloz, van arman
+/// @notice Interface for the royalty guard with all the fields, errors, functions, etc.
 interface IRoyaltyGuard {
+
+  /*//////////////////////////////////////////////////////////////////////////
+                              Enums
+  //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice An enum denoting 3 types for a list: OFF (0), ALLOW (1), DENY (2)
     enum ListType {
-        ALLOW,
-        DENY,
-        OFF
+      OFF,
+      ALLOW,
+      DENY
     }
 
-    error Unauthorized();
-    error CantAddToOFFList();
-    error DeadmanTriggerStillActive();
+  /*//////////////////////////////////////////////////////////////////////////
+                          Custom Errors
+  //////////////////////////////////////////////////////////////////////////*/
 
-    
+  /// @notice Emitted when an unauthorized party tries to call a specific function.
+  error Unauthorized();
+
+  /// @notice Emitted when trying to add an address to a list with type OFF.
+  error CantAddToOFFList();
+
+  /// @notice Emitted when the deadman trigger datetime threshold hasnt passed but tries to get called.
+  error DeadmanTriggerStillActive();
+
+  /// @notice Emitted when an admin only function tries to be called by a non-admin.
+  error MustBeAdmin();
+
+  /*//////////////////////////////////////////////////////////////////////////
+                          External Write Functions
+  //////////////////////////////////////////////////////////////////////////*/
+
+  /// @notice Toggles the list type between ALLOW, DENY, or OFF
+  /// @param _newListType to be applied to the list. Options are 0 (OFF), 1 (ALLOW), 2 (DENY)
+  function toggleListType(IRoyaltyGuard.ListType _newListType) external;
+
+  /// @notice Adds a list of addresses to the specified list.
+  /// @param _listType that addresses are being added to
+  /// @param _addrs being added to the designated list
+  function batchAddAddressToRoyaltyList(IRoyaltyGuard.ListType _listType, address[] calldata _addrs) external;
+
+  /// @notice Removes a list of addresses to the specified list.
+  /// @param _listType that addresses are being removed from
+  /// @param _addrs being removed from the designated list
+  function batchRemoveAddressToRoyaltyList(IRoyaltyGuard.ListType _listType, address[] calldata _addrs) external;
+
+  /// @notice Sets the deadman list trigger for the specified number of years from current block timestamp
+  /// @param _numYears to renew the trigger for.
+  function setDeadmanListTriggerDatetime(uint256 _numYears) external;
+
+  /// @notice Clears an entire list.
+  /// @param _listType of list being cleared.
+  function clearList(IRoyaltyGuard.ListType _listType) external;
+
+  /// @notice Triggers the deadman switch for the list
+  function activateDeadmanListTrigger() external;
+
+  /*//////////////////////////////////////////////////////////////////////////
+                          External Read Functions
+  //////////////////////////////////////////////////////////////////////////*/
+
+  /// @notice Returns the set of addresses on a list.
+  /// @param _listType of list being retrieved.
+  /// @return list of addresses on a given list.
+  function getList(IRoyaltyGuard.ListType _listType) external view returns (address[] memory);
+
+  /// @notice Returns the set of addresses on a list.
+  /// @return list of addresses on a given list.
+  function getInUseList() external view returns (address[] memory);
+
+  /// @notice Returns if the supplied operator address in part of the current in use list.
+  /// @param _operator address being checked.
+  /// @return bool relating to if the operator is on the list.
+  function isOperatorInList(address _operator) external view returns (bool);
+  
+
+  /// @notice States whether or not an address has admin permission.
+  /// @return bool denoting if _addr has admin permission.
+  function hasAdminPermission(address _addr) external view returns (bool);
+
+  /// @notice Returns the ListType currently being used;
+  /// @return ListType of the list. Values are: 0 (OFF), 1 (ALLOW), 2 (DENY)
+  function getListType() external view returns (ListType);
+
+  /// @notice The datetime threshold after which the deadman trigger can be called by anyone.
+  /// @return uint256 denoting unix epoch time after which the deadman trigger can be activated.
+  function getDeadmanTriggerAvailableDatetime() external view returns (uint256);
 }
